f1492c17e6cf41fccb5db1e4831345affa742293
Stanislav Lukyanchikov <lukyanchikov.si@gmail.com>
2022-11-07 17:59:45 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+        var dog = 8;
+        System.out.println(dog);
+
+        var cat = 3.6f;
+        System.out.println(cat);
+
+        var paper = 763789;
+        System.out.println(paper);
     }
 }
